4170a4c93f28457d79dddf96d0a397696029d7d3
Dan Dan Danii <168102843+KitsuHaru@users.noreply.github.com>
2024-05-04 16:07:41 +0700

    ElectronicsStore.java

--- src/Main/ElectronicsStore.java
+++ src/Main/ElectronicsStore.java
@@ -8,17 +8,9 @@ package Main;
  *
  * @author Ramdani
  */
-public class ElectronicsStore 
+public abstract class ElectronicsStore
 {
 
-  public static void main(String[] args) 
-  {
-    Television tv = new Television("Samsung", 55);
-    Laptop laptop = new Laptop("Apple", "MacBook Pro", 13);
-    Smartphone phone = new Smartphone("Google", "Pixel 6", 6.4);
-    
-    System.out.println(tv.getBrand() + " TV (" + tv.getSize() + " inches): " + tv.getDescription());
-    System.out.println(laptop.getBrand() + " " + laptop.getModel() + " Laptop (" + laptop.getSize() + " inches): " + laptop.getDescription());
-    System.out.println(phone.getBrand() + " " + phone.getModel() + " Smartphone (" + phone.getSize() + " inches): " + phone.getDescription());
-  }
+  // Abstract method to be implemented by subclasses to describe the product
+  public abstract String getDescription();
 }
